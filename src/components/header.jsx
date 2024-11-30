af37f257e14db0e59301916f31543ba8e19e4c7b
RohanPanchal-21 <panchalrohan64@gmail.com>
2024-11-30 21:04:32 +0530

    My new changes for the file in the para

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,10 +8,12 @@ const header = () => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rem
         commodi perferendis vitae vel suscipit! Commodi, cupiditate vero
         reprehenderit beatae fuga temporibus. Nam recusandae nostrum aliquid
-        consectetur optio, adipisci atque.
+        consectetur optio, adipisci atque. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Fuga, totam. Veritatis fugiat, dolore vero
+        quas excepturi modi maxime laborum neque porro perspiciatis! Cum,
+        deserunt. Perferendis autem a magni blanditiis dolore.
       </p>
     </Fragment>
   );
 };
-
 export default header;
