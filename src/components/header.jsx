f3852267d0d4f07fe249c9e07cec2864c9563905
RohanPanchal-21 <panchalrohan64@gmail.com>
2024-11-30 20:51:08 +0530

    Added more sentences to the paragraph

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,11 @@ const header = () => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rem
         commodi perferendis vitae vel suscipit! Commodi, cupiditate vero
         reprehenderit beatae fuga temporibus. Nam recusandae nostrum aliquid
-        consectetur optio, adipisci atque.
+        consectetur optio, adipisci atque. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Harum magnam, quasi sapiente reiciendis
+        modi dignissimos asperiores laudantium, facere perspiciatis fuga
+        inventore officia praesentium. A voluptatem magni deserunt nam vero
+        iste!
       </p>
     </Fragment>
   );
