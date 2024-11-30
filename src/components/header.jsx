b221a9c58b9f77e400d2a8d191cb1d9b94e31437
RohanPanchal-21 <panchalrohan64@gmail.com>
2024-11-30 20:43:56 +0530

    made changes to the header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const header = () => {
-  return <div>header</div>;
+  return (
+    <Fragment>
+      <div>Header</div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rem
+        commodi perferendis vitae vel suscipit! Commodi, cupiditate vero
+        reprehenderit beatae fuga temporibus. Nam recusandae nostrum aliquid
+        consectetur optio, adipisci atque.
+      </p>
+    </Fragment>
+  );
 };
 
 export default header;
